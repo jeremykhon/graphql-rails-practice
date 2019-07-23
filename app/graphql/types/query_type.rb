114052e2df735f6acfddd45039acf3849a2484ca
Jeremy Hon <jehon11@gmail.com>
2019-07-23 22:46:31 +0900

    find link query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,10 +3,18 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
     field :all_links, [LinkType], null: false, description: 'Retrieve all links'
+    field :find_link, LinkType, null: false do
+      description 'Find a post by ID'
+      argument :id, ID, required: true
+    end
 
     # this method is invoked, when `all_link` fields is being resolved
     def all_links
       Link.all
+    end
+
+    def find_link(id:)
+      Link.find(id)
     end
   end
 end
